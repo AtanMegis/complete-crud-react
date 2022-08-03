2011b0c212a7d5dcdcbb8e602cddba1380f9c26e
megis <atanmegis.cn@gmail.com>
2022-08-03 19:46:01 +0700

    feat: google authentication

---
+++ client/src/context/ContextProvider.js
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useEffect, useReducer } from 'react';
+import reducer from './reducer';
+
+const INITIAL_STATE = {
+	currentUser: null,
+	openLogin: false,
+	loading: false,
+	alert: { open: false, severity: 'info', message: '' },
+};
+
+const Context = createContext(INITIAL_STATE);
+
+export const useValue = () => {
+	return useContext(Context);
+};
+
+const ContextProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+	useEffect(() => {
+		const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+		if (currentUser) {
+			dispatch({ type: 'UPDATE_USER', payload: currentUser });
+		}
+	}, []);
+
+	return (
+		<Context.Provider value={{ state, dispatch }}>
+			{children}
+		</Context.Provider>
+	);
+};
+
+export default ContextProvider;
