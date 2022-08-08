7e85fdbc77e81788fbba95930a09317bfbe7f49e
megis <atanmegis.cn@gmail.com>
2022-08-08 16:18:21 +0700

    feat: location, details, image page

--- client/src/context/ContextProvider.js
+++ client/src/context/ContextProvider.js
@@ -1,30 +1,31 @@
-import React, { createContext, useContext, useEffect, useReducer } from 'react';
+import { createContext, useContext, useEffect, useReducer } from 'react';
 import reducer from './reducer';
 
-const INITIAL_STATE = {
+const initialState = {
 	currentUser: null,
 	openLogin: false,
 	loading: false,
 	alert: { open: false, severity: 'info', message: '' },
 	profile: { open: false, file: null, photoURL: '' },
 	images: [],
+	details: { title: '', description: '', price: 0 },
+	location: { lng: 0, lat: 0 },
 };
 
-const Context = createContext(INITIAL_STATE);
+const Context = createContext(initialState);
 
 export const useValue = () => {
 	return useContext(Context);
 };
 
 const ContextProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+	const [state, dispatch] = useReducer(reducer, initialState);
 	useEffect(() => {
 		const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 		if (currentUser) {
 			dispatch({ type: 'UPDATE_USER', payload: currentUser });
 		}
 	}, []);
-
 	return (
 		<Context.Provider value={{ state, dispatch }}>
 			{children}
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -32,6 +32,13 @@ const reducer = (state, action) => {
 					(image) => image !== action.payload
 				),
 			};
+		case 'UPDATE_DETAILS':
+			return {
+				...state,
+				details: { ...state.details, ...action.payload },
+			};
+		case 'UPDATE_LOCATION':
+			return { ...state, location: action.payload };
 
 		default:
 			throw new Error('No matched action!');
